7b7391d4ab0902be4eb99e835166eca841b0c1c2
analog_wizard <thecoolmathdude@gmail.com>
2023-06-01 10:59:06 -0500

    Working concept thanks to a modified version of https://github.com/theasp/docker-novnc, mupen needs to be manually launch and a system needs to be put in place to assign players, to play audio, and to connect with client side controllers (I dont have any, but a theoretical implementation at least).

--- util/proxy.sh
+++
@@ -1,7 +0,0 @@
-set -x
-
-sed_string=$(ruby gensed.rb)
-#sed_string="s/%04%00%00%00%00%00%ff%52/%04%00%00%00%00%00%ff%54/I"
-# Proxy and modify packets in-flight
-port=`expr 5900 + $PLAYER_NUM`
-netsed tcp $port $SERVER 5900 $sed_string | grep -v "Caught" | grep -v "Forwarding"
